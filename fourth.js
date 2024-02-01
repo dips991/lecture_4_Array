0e843a429f60265e06328342cef89701da52b20f
Dipak Deshmukh <121744826+dips991@users.noreply.github.com>
2024-02-01 18:02:42 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,45 @@
+// array
+//let marks=[1,2,3,4,5];
+// console.log(marks);
+// console.log(marks.length);
+//marks[0]=5;
+//console.log(marks);
+
+//string -inmutable
+//array- mutable
+
+//looping over an Array
+// let heroes=["ironman", "thor", "spidarman","abc","dk","xyz"];
+//    for(let idx=0;idx<heroes.length;idx++){
+//     console.log(heroes[idx]);
+//    }
+//for-of prefer 
+// for(let hero of heroes){
+//     console.log(hero);
+// }
+// Que
+// let marks=[85,34,56,78,98];
+// let sum=0;
+// for(let val of marks){
+//     sum+=val;
+// }
+// let avg=sum/marks.length;
+// console.log("avg  marks of class is: ", avg);
+
+//que2
+// let items=[250,645,300,900,50];
+// let i=0;
+// for(let val of items){
+//     let offer=val/10;
+//     items[i]-=offer;
+//     i++;
+// }
+// console.log(items);
+
+//Array method
+//push pop tostring
+
+// let aee=[1,2,3,4,,5,6];
+// //aee.splice(2,2,101,102);
+// //add ele
+// aee.splice(2,0,101);
